af0e0f1469670d0542d0400d424083e29ac0b348
miltonbolonha <miltonbolonha@gmail.com>
2022-04-28 10:04:45 -0300

    bugfix/sic cite https insecure protocol

--- gatsby-theme-boilerplate-blog/src/pages/componentes.js
+++ gatsby-theme-boilerplate-blog/src/pages/componentes.js
@@ -12,19 +12,35 @@ const Componentes = ({ location }) => {
         dispositivo eletrônico.
       </p>
       <p>
-        Existém <em>"coisas"</em> nesse mundo que são <em>"algo"</em> apenas
-        porque fazem parte de um todo. Esses mecanismos são chamados de
-        componentes e são encontrados nos mais diversos ambientes.
+        Existém <em>"coisas"</em> nesse mundo que são somente se tornam{' '}
+        <em>"algo"</em> porque fazem parte de um todo, sem o todo não haveria
+        esse algo. Esses mecanismos atuantes dentro apenas de seus escopos, são
+        chamados de componentes e são encontrados nos mais diversos ambientes.
       </p>
       <h2>O que são componentes?</h2>
       <p>
         Um componente é parte de um módulo. Serve para reutilizar códigos,
-        mudando apenas o que é necessário dentro de uma repetição inteligente. É
-        comum encontrar componentes visuais, mas também existem componentes que
-        são mais complexos, como por exemplo, o componente de formulário.
+        mudando apenas o que é necessário dentro de uma repetição inteligente.
+        Componentes podem ser visuais, mas também existem componentes que são
+        mais complexos, como por exemplo, o componente de formulário.
       </p>
-
-      <blockquote cite="http://developer.mozilla.org">
+      <p>
+        Agora que você já aprendeu um pouco mais sobre componentes, veja abaixo
+        a lista dos compontes do <strong>Boilerplate Blog</strong>:
+      </p>
+      <ul>
+        <li>Body</li>
+        <li>Header</li>
+        <li>Heading's</li>
+        <li>Main page template</li>
+        <li>Main template wrapper</li>
+        <li>New footer block</li>
+        <li>Page header</li>
+        <li>Post card</li>
+        <li>Posts block</li>
+        <li>Single post block</li>
+      </ul>
+      <blockquote cite="Zen brush">
         <p>Zumba frenética.</p>
       </blockquote>
     </MainPageWrapper>
