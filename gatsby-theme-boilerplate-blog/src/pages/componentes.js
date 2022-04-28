5e29b6a73a2fb882d187f1e3003219db92ff1da1
miltonbolonha <miltonbolonha@gmail.com>
2022-04-28 12:22:59 -0300

    feature/pages styles added

--- gatsby-theme-boilerplate-blog/src/pages/componentes.js
+++ gatsby-theme-boilerplate-blog/src/pages/componentes.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import MainPageWrapper from '@BlockBuilder/MainPageWrapper'
 import { defaultSchema } from '../configs/schemas'
 
 const Componentes = ({ location }) => {
   return (
-    <MainPageWrapper seoSchema={defaultSchema(location)} title="Componentes">
+    <MainPageWrapper
+      seoSchema={defaultSchema(location)}
+      title="Componentes"
+      classes="main-page-wrapper"
+    >
       <p>
         O mundo da programação e da internet pode ser resumido como um gigante
-        serviço de correspondência. Cartas são enviadas para todos os lados na
-        velocidade da luz contendo textos que serão interpretados por um
-        dispositivo eletrônico.
+        serviço de correspondências. <br />
+        Cartas são enviadas para todos os lados na velocidade da luz contendo
+        textos que serão interpretados por um dispositivo eletrônico.
       </p>
       <p>
-        Existém <em>"coisas"</em> nesse mundo que são somente se tornam{' '}
+        Existém <em>"coisas"</em> nesse mundo que somente se tornam{' '}
         <em>"algo"</em> porque fazem parte de um todo, sem o todo não haveria
-        esse algo. Esses mecanismos atuantes dentro apenas de seus escopos, são
-        chamados de componentes e são encontrados nos mais diversos ambientes.
+        esse algo. <br />
+        Esses mecanismos atuam dentro apenas de seus escopos, chamados de
+        componentes, são encontrados nos mais diversos tipos de ambientes.
       </p>
       <h2>O que são componentes?</h2>
       <p>
@@ -43,6 +49,45 @@ const Componentes = ({ location }) => {
       <blockquote cite="Zen brush">
         <p>Zumba frenética.</p>
       </blockquote>
+      <h2>Vamos falar mais sobre componentes?</h2>
+      <p>
+        Dentro do sistema <strong>Te[i]as</strong> de classificação cibernética
+        os componentes NÃO guardam códigos contendo lógicas, apenas a parte
+        visual, ou seja a saída visual da lógica é que são escritas em
+        componentes.
+      </p>
+
+      <p>
+        Fossem apenas{' '}
+        <sup>
+          <a name="ref1" href="#myfootnote1" id="ref1">
+            1
+          </a>
+        </sup>{' '}
+        componentes visuais não necessitariam serem escritos em código
+        dinâmicos, pois a função deles é visual e não de inteligência. Os
+        componentes não possuem a lógica em si e geralmente também não possuem a
+        informação. Sem informação e sem lógica, resta uma estrutura inteligente
+        que fará o invólucro de qualquer informação corretamente padronizada.
+      </p>
+      <h2>Componentes e containers, quem é quem?</h2>
+      <p>
+        Essa estrutura é dinâmica e tem em sua cabeça do tempo a lógica guardada
+        em uma pasta nomeada de <em>containers</em>.
+      </p>
+      <p>
+        Os <em>containers</em> geralmente chamam como retorno de suas próprias
+        chamadas um componente, mas não carrega o seu código, pois a função dos
+        containers é guardar os códigos contendo as lógicas de manipulação de
+        dados. Por sua vez, o componente separado da lógica, funciona como uma
+        carroceria acoplada a inteligência mecânica de um carro.
+      </p>
+      <sup id="myfootnote1" name="myfootnote1">
+        1. Some: Existe sim.{' '}
+        <a href="#ref1" title="Voltar para footnote 1.">
+          ↩
+        </a>
+      </sup>
     </MainPageWrapper>
   )
 }
